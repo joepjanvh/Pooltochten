38b20ab0f929f246a8fdb0ad7261da7b4ae703c1
JoepJan van Hernen <joepjanvh@gmail.com>
2022-11-07 19:50:40 +0100

    first upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,54 @@
+<x-app-layout>
+  
+<div class="p-2">  </div>
+<div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:shrink-0" >
+      <img class="p-8 h-52 rounded-full object-cover w-52" src="/images/img.jpg" alt="Profile">
+    </div>
+    <div class="p-8">
+      <div class="uppercase p-2 tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+          <p class="mt-2 p-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+          <div class="flex">
+            <div class="p-2 "><strong>123</strong> Posts</div>
+            <div class="p-2"><strong>123</strong> Followers</div>
+            <div class="p-2"><strong>123</strong> Following</div>
+        </div>
+    
+    
+    </div>
+    
+  </div>
+     <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-3 ">
+     <div class="col-span-1"><img class= "w-48 h-48 border p-2" src="
+    https://scontent-ams4-1.cdninstagram.com/v/t51.2885-15/24838514_1926051427711833_7703315623979253760_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=109&_nc_ohc=IUc8q7NThZkAX9fjkTP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAp36hrLnS4kOaym2lUCS63vnwH0pTDWrg7woy5a2NB_A&oe=636F1EE2&_nc_sid=8fd12b"></div>
+<div class="col-span-1"><img class= "w-48 h-48 border p-2" src="
+    https://scontent-ams4-1.cdninstagram.com/v/t51.2885-15/24838514_1926051427711833_7703315623979253760_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=109&_nc_ohc=IUc8q7NThZkAX9fjkTP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAp36hrLnS4kOaym2lUCS63vnwH0pTDWrg7woy5a2NB_A&oe=636F1EE2&_nc_sid=8fd12b"></div>
+    <div class="col-span-1"><img class= "w-48 h-48 border p-2 p-2" src="
+    https://scontent-ams4-1.cdninstagram.com/v/t51.2885-15/24838514_1926051427711833_7703315623979253760_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=109&_nc_ohc=IUc8q7NThZkAX9fjkTP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAp36hrLnS4kOaym2lUCS63vnwH0pTDWrg7woy5a2NB_A&oe=636F1EE2&_nc_sid=8fd12b"></div>
+    <div class="col-span-1"><img class= "w-48 h-48 border p-2" src="
+    https://scontent-ams4-1.cdninstagram.com/v/t51.2885-15/24838514_1926051427711833_7703315623979253760_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=109&_nc_ohc=IUc8q7NThZkAX9fjkTP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAp36hrLnS4kOaym2lUCS63vnwH0pTDWrg7woy5a2NB_A&oe=636F1EE2&_nc_sid=8fd12b"></div>
+    <div class="col-span-1"><img class= "w-48 h-48 border p-2" src="
+    https://scontent-ams4-1.cdninstagram.com/v/t51.2885-15/24838514_1926051427711833_7703315623979253760_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=109&_nc_ohc=IUc8q7NThZkAX9fjkTP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAp36hrLnS4kOaym2lUCS63vnwH0pTDWrg7woy5a2NB_A&oe=636F1EE2&_nc_sid=8fd12b"></div>
+    <div class="col-span-1"><img class= "w-48 h-48 border p-2" src="
+    https://scontent-ams4-1.cdninstagram.com/v/t51.2885-15/24838514_1926051427711833_7703315623979253760_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=109&_nc_ohc=IUc8q7NThZkAX9fjkTP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAp36hrLnS4kOaym2lUCS63vnwH0pTDWrg7woy5a2NB_A&oe=636F1EE2&_nc_sid=8fd12b"></div>
+        <div class="col-span-1"><img class= "w-48 h-48 border p-2" src="
+    https://scontent-ams4-1.cdninstagram.com/v/t51.2885-15/24838514_1926051427711833_7703315623979253760_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=109&_nc_ohc=IUc8q7NThZkAX9fjkTP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAp36hrLnS4kOaym2lUCS63vnwH0pTDWrg7woy5a2NB_A&oe=636F1EE2&_nc_sid=8fd12b"></div>
+
+         <div class="col-span-1"><img class= "w-48 h-48 border p-2" src="
+    https://scontent-ams4-1.cdninstagram.com/v/t51.2885-15/24838514_1926051427711833_7703315623979253760_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=109&_nc_ohc=IUc8q7NThZkAX9fjkTP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAp36hrLnS4kOaym2lUCS63vnwH0pTDWrg7woy5a2NB_A&oe=636F1EE2&_nc_sid=8fd12b"></div>
+
+         <div class="col-span-1"><img class= "w-48 h-48 border p-2" src="
+    https://scontent-ams4-1.cdninstagram.com/v/t51.2885-15/24838514_1926051427711833_7703315623979253760_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent-ams4-1.cdninstagram.com&_nc_cat=109&_nc_ohc=IUc8q7NThZkAX9fjkTP&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAp36hrLnS4kOaym2lUCS63vnwH0pTDWrg7woy5a2NB_A&oe=636F1EE2&_nc_sid=8fd12b"></div>
+         
+     
+     
+     
+     </div>
+     
+
+
+</div>
+</div>
+
+</x-app-layout>
